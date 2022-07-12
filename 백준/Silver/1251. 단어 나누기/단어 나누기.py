25eeba1f3910de2a6be7285ac8d124c5218985ca
CatJelly <70837543+CatJelly@users.noreply.github.com>
2022-07-13 00:55:51 +0900

    [Silver V] Title: 단어 나누기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,18 +1,11 @@
-word = list(input())
-answer = []
-tmp = []
+word = input()
+word_list = []
 
-for i in range(1, len(word) - 1):
-    for j in range(i + 1, len(word) ):
-        a = word[:i]
-        b = word[i:j]
-        c = word[j:]
-        a.reverse()
-        b.reverse()
-        c.reverse()
-        tmp.append(a + b + c)
+for i in range(1, len(word) - 2):
+    part1 = word[0:i]
+    for j in range(i + 1, len(word)):
+        part2 = word[i:j]
+        part3 = word[j:]
+        word_list.append(''.join(reversed(part1)) + ''.join(reversed(part2)) + ''.join(reversed(part3)))
 
-for a in tmp:
-    answer.append(''.join(a))
-
-print(sorted(answer)[0])
+print(sorted(word_list)[0])

29b2e70ac8c7a621943b7281c22f44dd3a59fa06
CatJelly <70837543+CatJelly@users.noreply.github.com>
2022-08-27 19:37:16 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -0,0 +1,10 @@
+def Rev(X):
+    X = str(X)
+    res = ""
+    for i in range(len(X) - 1, -1, -1):
+        res += X[i]
+    return int(res)
+
+X, Y = map(int, input().split())
+
+print(Rev(Rev(X) + Rev(Y)))
